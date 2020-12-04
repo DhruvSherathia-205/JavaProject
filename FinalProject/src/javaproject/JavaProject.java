0d7ce92f9c0cb6b3bb88ace533a7df75b211370e
Mahima Mulani <Mahima Mulani@192.168.221.1>
2020-12-04 18:08:26 -0500

    done

--- FinalProject/src/javaproject/JavaProject.java
+++ FinalProject/src/javaproject/JavaProject.java
@@ -14,7 +14,7 @@ public class JavaProject {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 			System.out.println("Dhruv Sherathia 205");
-			System.out.println("Mahima Mulani 508");
+			System.out.println("Mahima Mulani hi 508");
 			System.out.println("Malav Patel 213");
 			//Added
 			System.out.print("Swapnil Desai 010");
