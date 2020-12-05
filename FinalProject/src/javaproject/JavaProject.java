318eaf874e0445970529a359fe46b5736827eb67
Dhruv Sherathia 205 <c0797205@mylambton.ca>
2020-12-05 16:27:51 +0530

    Changed settings to no assume unchanged!

--- FinalProject/src/javaproject/JavaProject.java
+++ FinalProject/src/javaproject/JavaProject.java
@@ -13,12 +13,12 @@ public class JavaProject {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
 			System.out.println("Dhruv Sherathia 205");
 			System.out.println("Mahima Mulani 508");
-			System.out.println("Malav Patel 213");
-			System.out.println("Sukhdeep Shabla 729");
-			System.out.println("Swapnil Desai 010"); 
+			System.out.println("Malav Patel 213"); 
+			System.out.println("Sukhdeep Shabla 729"); 
+			System.out.println("Swapnil Desai 010 ");  
 	}
 
 }
