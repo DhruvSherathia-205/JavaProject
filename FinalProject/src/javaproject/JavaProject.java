3212737b9b48b18f1b474d089f5bc028c46b7e61
Dhruv Sherathia 205 <c0797205@mylambton.ca>
2020-12-05 15:41:41 +0530

    commit!
    Signed-off-by: Dhruv Sherathia 205 <c0797205@mylambton.ca>

--- FinalProject/src/javaproject/JavaProject.java
+++ FinalProject/src/javaproject/JavaProject.java
@@ -17,8 +17,8 @@ public class JavaProject {
 			System.out.println("Dhruv Sherathia 205");
 			System.out.println("Mahima Mulani 508");
 			System.out.println("Malav Patel 213");
+			System.out.println("Sukhdeep Shabla 729");
 			System.out.println("Swapnil Desai 010");
-			System.out.println("add yours too");
 	}
 
 }
